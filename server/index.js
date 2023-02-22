6dca5e138f984d63ee57d1858507850e52477726
Miles <96042491+Miles-PD@users.noreply.github.com>
2023-02-22 00:55:05 -0800

    started backend

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import connectDB from './mongodb/connect.js';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import * as dotenv from 'dotenv';
+
+// ** configs
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.send({ message: "Hello" })
+})
+
+const serverInit = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, console.log('server on 8080'))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+serverInit();
