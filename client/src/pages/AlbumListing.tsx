574e30f8526b6070f37e98164ba5e6c4e243b6b7
Miles <96042491+Miles-PD@users.noreply.github.com>
2023-03-03 04:04:45 -0800

    more backend work for swiper component, swiper import bug needs to be fixed

--- client/src/pages/AlbumListing.tsx
+++ client/src/pages/AlbumListing.tsx
@@ -1,17 +1,74 @@
 import { useParams } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import axios, { AxiosResponse } from 'axios'
+import { useEffect, useState } from "react";
 
 import AlbumDetails from "../components/AlbumDetails"
+import AlbumCard from "../components/AlbumCard";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from "swiper";
+
+import 'swiper/swiper.min.css';
+import 'swiper/css/freemode';
 
 
 
 const AlbumListing = () => {
 
+    const [otherAlbums, setOtherAlbums] = useState<AxiosResponse | null | any | void>([]);
+
     const { id } = useParams<{ id?: string }>(); 
     console.log(id)
 
+    useEffect(() => {
+
+        const fetchOtherAlbums = async () => {
+            try {
+                const others = await axios.get(`http://localhost:8080/api/v1/artist/more/${id}`);
+                
+                setOtherAlbums(others.data);
+            } catch (error) {
+                console.log("Error finding album details:", error);
+              }
+        }
+
+        fetchOtherAlbums();
+    }, [])
+
     return (
-        
-        <AlbumDetails id={id ?? ""} />
+        <>
+            <AlbumDetails id={id ?? ""} />
+
+            <div className="w-full flex flex-col py-[15px] px-[50px]">
+                <Swiper
+                    spaceBetween={25}
+                    slidesPerView="auto"
+                    freeMode
+                    centeredSlides
+                    centeredSlidesBounds
+                    modules={[FreeMode]}
+                    className="mt-4"
+                >
+                    {otherAlbums?.map((album: any, i: number) => {
+                        <SwiperSlide key={i}>
+                            <AlbumCard 
+                                key={album._id}
+                                _id={album._id}
+                                title={album.title} 
+                                artist={album.artist}
+                                label={album.label}
+                                genre={album.genre} 
+                                khz={album.khz} 
+                                bitrate={album.bitrate}
+                                numTracks={album.numTracks}
+                                runtime={album.runtime}
+                            />
+                        </SwiperSlide>
+                    })}
+                </Swiper>
+            </div>
+        </>
     )
 }
 
