154cf0ca870f65318e2863cb948c68631f68e2c8
Miles <96042491+Miles-PD@users.noreply.github.com>
2023-03-22 04:21:14 -0700

    starting on right side of home box

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -36,7 +36,6 @@ const Home = () => {
                                 effect="fade"
                                 width={696} // set a fixed width
                                 height={384} // set a fixed height
-                                
                                 >
                                 <SwiperSlide key={0} className="overflow-hidden">
                                     <img src={igloo} className="h-full w-full object-none" />
@@ -48,8 +47,8 @@ const Home = () => {
                         </div>
 
                         <section className='opacityOverlay group group-hover:relative z-10'>
-                            <div className="absolute left-0 w-full h-full group-hover:block z-2">
-                                <div className="hidden absolute bottom-0 left-0 w-2/4 h-40 bg-black backdrop-blur-sm animate-slideup_faster opacity-75 group-hover:block" />
+                            <div className="absolute left-0 w-2/4 h-full group-hover:block z-2">
+                                <div className="hidden absolute bottom-0 left-0 w-full h-40 bg-black backdrop-blur-sm animate-slideup_faster opacity-75 group-hover:block" />
                             </div>
                                 <div className='opacity-0 px-3 pb-3 pt-8 absolute bottom-0 left-0 w-2/4 h-2/4 overflow-hidden group-hover:opacity-100 ease-in-out'>
                                     <p className='text-white pt-1 text-2xl'>
@@ -61,8 +60,8 @@ const Home = () => {
                                 </div>
                         </section>
                     </div>
-                    <div className="w-2/4 bg-green-400">
-                        
+                    <div className="absolute right-0 float-right w-2/4 bg-green-400">
+                        <div className="w-1/4 h-full bg-red-200">test</div>
                     </div>
                 </div>
             </div>
